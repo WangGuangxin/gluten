4d89941d7a165c77cb35b8fc7164efb76318492c
Zac Blanco <zachary.blanco@bytedance.com>
2026-05-06 21:29:54 +0000

    Fix bugs with Spark 3.2 paion shim Impl

--- gluten-paimon/src-paimon-spark32/main/java/org/apache/gluten/execution/PaimonSparkShimImpl.scala
+++ gluten-paimon/src-paimon-spark32/main/java/org/apache/gluten/execution/PaimonSparkShimImpl.scala
@@ -20,18 +20,16 @@ import org.apache.paimon.data.InternalRow
 import org.apache.paimon.io.DataFileMeta
 import org.apache.paimon.spark.PaimonScan
 import org.apache.paimon.table.FileStoreTable
-import org.apache.paimon.table.source.{ChainDataSplit, DataSplit}
-import org.apache.paimon.types.{DateType, RowType}
+import org.apache.paimon.table.source.DataSplit
+import org.apache.paimon.types.RowType
 import org.apache.paimon.utils.InternalRowPartitionComputer
-import org.apache.spark.sql.catalyst.util.DateFormatter
 
-import java.util
 import scala.collection.JavaConverters.asScalaBufferConverter
 
 class PaimonSparkShimImpl extends PaimonSparkShim {
 
   override def isChainSplit(split: DataSplit): Boolean = {
-    split.isInstanceOf[ChainDataSplit]
+    false
   }
 
   override def getSplitPartition(split: DataSplit): InternalRow = {
@@ -39,19 +37,7 @@ class PaimonSparkShimImpl extends PaimonSparkShim {
   }
 
   override def getBucketPath(split: DataSplit, file: DataFileMeta): String = {
-    val isChainDataSplit: Boolean = isChainSplit(split)
-    val bucketPath = if (isChainDataSplit) {
-      split
-        .asInstanceOf[ChainDataSplit]
-        .fileBucketPathMapping()
-        .get(file.fileName())
-    } else {
       split.bucketPath()
-    }
-    if (isChainDataSplit && bucketPath == null) {
-      throw new RuntimeException(s"Bucket path is null for file ${file.fileName()}")
-    }
-    bucketPath
   }
 
   override def getInternalPartitionComputer(paimonScan: PaimonScan): InternalRowPartitionComputer = {
@@ -70,18 +56,4 @@ case class PaimonPartitionComputer(paimonScan: PaimonScan, paimonRowType: RowTyp
     paimonRowType,
     paimonPartitionKeys,
     false) {
-
-  override def generatePartValues(
-                                   in: org.apache.paimon.data.InternalRow): util.LinkedHashMap[String, String] = {
-    val result = super.generatePartValues(in)
-    val getters = rowType.fieldGetters()
-    for (i <- 0 until getters.length) {
-      rowType.getTypeAt(i) match {
-        case _: DateType =>
-          result.put(paimonPartitionKeys(i), DateFormatter().format(in.getInt(i)))
-        case _ =>
-      }
-    }
-    result
-  }
 }
